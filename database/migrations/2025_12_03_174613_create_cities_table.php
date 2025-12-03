913fa282cd5d70d6edad9a753f20edb0487e551a
gsokolowski <gregor.sokolowski@gmail.com>
2025-12-03 19:26:28 +0100

    County and City factory and seeding db

--- database/migrations/2025_12_03_174613_create_cities_table.php
+++ database/migrations/2025_12_03_174613_create_cities_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('country_id')->constrained('countries')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('countries');
+        Schema::dropIfExists('cities');
     }
 };
