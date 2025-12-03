f197af8e6be02921054a72ad1cbbfcf6a801e5e4
Greg <greg@mac.home>
2025-12-03 19:26:28 +0100

    County and City factory and seeding db

---
+++ app/Models/City.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class City extends Model
+{
+    /** @use HasFactory<\Database\Factories\CityFactory> */
+    use HasFactory;
+    
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+}
--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\City;
+use App\Models\Country;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,13 @@ class CountrySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Create 10 countries and associate random number of cities with each country
+        Country::factory()->count(10)->create()
+            ->each(function ($country) {
+                City::factory()
+                    ->count(rand(3, 10))
+                    ->for($country)
+                    ->create();
+            });
     }
 }
