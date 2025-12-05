b4da5e1addc8922d01ce5024767b908457e0bfc7
gsokolowski <gregor.sokolowski@gmail.com>
2025-12-05 14:53:34 +0100

    reservation_room remove id from migration

--- database/migrations/2025_12_04_211610_create_reservation_room_table.php
+++ database/migrations/2025_12_04_211610_create_reservation_room_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // this is pivot  table between reservations and rooms, no need for id
         Schema::create('reservation_room', function (Blueprint $table) {
-            $table->id();
             // Foreign key to reservations table
             $table->foreignId('reservation_id')->constrained('reservations')->onDelete('cascade');
             // Foreign key to rooms table
             $table->foreignId('room_id')->constrained('rooms')->onDelete('cascade');
-            $table->boolean('status')->default(false);
+            $table->boolean('status')->default(false); // status is reservation confirmed or not
             // Prevent duplicate room assignments for the same reservation
             $table->unique(['reservation_id', 'room_id']); 
         });
